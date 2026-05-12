f7c5db30049cb6d0734cec8926208d59ad00f38e
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-12 16:50:57 +0700

    Auto-commit: 2026-05-12_16:50:57

--- protected/models/EventSports.php
+++ protected/models/EventSports.php
@@ -7,4 +7,27 @@ class EventSports extends BaseEventSports
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
+
+	public static function getByEventId($eventId)
+	{
+		$result = ApiClient::get(ApiEndpoints::EVENT_SPORT_LIST, array('event_id' => $eventId));
+		if ($result['success'] && isset($result['data']['data'])) {
+			return $result['data']['data'];
+		}
+		return array();
+	}
+
+	public static function storeViaApi($eventId, $sportId)
+	{
+		return ApiClient::post(ApiEndpoints::EVENT_SPORT_STORE, array(
+			'event_id' => $eventId,
+			'sport_id' => $sportId,
+		));
+	}
+
+	public static function deleteViaApi($id)
+	{
+		$url = ApiEndpoints::url(ApiEndpoints::EVENT_SPORT_DESTROY, array('id' => $id));
+		return ApiClient::delete($url);
+	}
 }
