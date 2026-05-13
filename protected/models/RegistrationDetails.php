a7c059ed4406d7786631469667bb3bc5f8ff34b5
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-13 08:21:35 +0700

    Auto-commit: 2026-05-13_08:21:35

--- protected/models/RegistrationDetails.php
+++ protected/models/RegistrationDetails.php
@@ -7,4 +7,24 @@ class RegistrationDetails extends BaseRegistrationDetails
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
+
+	public static function getByRegistrationId($registrationId)
+	{
+		$result = ApiClient::get(ApiEndpoints::REGISTRATION_DETAIL_LIST, array('registration_id' => $registrationId));
+		if ($result['success'] && isset($result['data']['data'])) {
+			return $result['data']['data'];
+		}
+		return array();
+	}
+
+	public static function storeViaApi($data)
+	{
+		return ApiClient::post(ApiEndpoints::REGISTRATION_DETAIL_STORE, $data);
+	}
+
+	public static function deleteViaApi($id)
+	{
+		$url = ApiEndpoints::url(ApiEndpoints::REGISTRATION_DETAIL_DESTROY, array('id' => $id));
+		return ApiClient::delete($url);
+	}
 }
