65e43f7e6a5c3f41cd5113596ce865071e4dd370
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-13 09:56:56 +0700

    Auto-commit: 2026-05-13_09:56:56

--- protected/modules/admin/views/registrationPeriods/_form.php
+++ protected/modules/admin/views/registrationPeriods/_form.php
@@ -121,11 +121,24 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/asse
 ?>
 <script>
 document.addEventListener('DOMContentLoaded', function() {
+    var Vietnamese = {
+        weekdays: {
+            shorthand: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+            longhand: ['Chủ nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy']
+        },
+        months: {
+            shorthand: ['Th1', 'Th2', 'Th3', 'Th4', 'Th5', 'Th6', 'Th7', 'Th8', 'Th9', 'Th10', 'Th11', 'Th12'],
+            longhand: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12']
+        },
+        firstDayOfWeek: 1
+    };
+
     var config = {
         enableTime: true,
         dateFormat: 'd-m-Y H:i',
         time_24hr: true,
-        allowInput: true
+        allowInput: true,
+        locale: Vietnamese
     };
 
     flatpickr('#start_time_picker', Object.assign({}, config, {
