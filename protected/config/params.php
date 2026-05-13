9d5343fad707cad1b3ce043e661cb30d7639116a
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-13 11:37:55 +0700

    Auto-commit: 2026-05-13_11:37:55

--- protected/config/params.php
+++ protected/config/params.php
@@ -11,6 +11,8 @@ return array(
     // Portal SSO Configuration
     'portal' => array(
         'url' => 'https://portal.muongthanh.vn',
+        'api_url' => 'https://api.portal.muongthanh.vn',
+        'sso_me_endpoint' => '/api/sso/me',
         'jwt_secret' => getenv('JWT_SECRET') ?: 'B7BFCA89BF11459E898B26310C2794E6819FB0AD0565B4C3',
         'jwt_algorithm' => 'HS256',
     ),
