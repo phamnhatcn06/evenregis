b35b313fa7e2074ecc3b26fab3da953c48237127
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-14 10:19:53 +0700

    Auto-commit: 2026-05-14_10:19:53

--- protected/modules/admin/views/registrations/view.php
+++ protected/modules/admin/views/registrations/view.php
@@ -215,6 +215,26 @@ $perColumn = ceil($totalAttrs / $columns);
     </div>
 </div>
 
+<!-- Modal View Document -->
+<div class="modal fade" id="documentModal" tabindex="-1">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Xem tài liệu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body p-0" id="documentModalBody" style="min-height:500px;">
+            </div>
+            <div class="modal-footer">
+                <a href="#" id="documentDownloadLink" class="btn btn-primary" download>
+                    <i class="fa fa-download me-1"></i>Tải xuống
+                </a>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Modal Reject -->
 <div class="modal fade" id="rejectModal" tabindex="-1">
     <div class="modal-dialog">
