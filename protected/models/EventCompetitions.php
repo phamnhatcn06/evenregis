7ebc9e41a2530c9a268d10ac0bf31e9aab06c7cf
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-14 14:25:39 +0700

    Auto-commit: 2026-05-14_14:25:39

--- protected/models/EventCompetitions.php
+++ protected/models/EventCompetitions.php
@@ -4,7 +4,31 @@ Yii::import('application.models._base.BaseEventCompetitions');
 
 class EventCompetitions extends BaseEventCompetitions
 {
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public static function getByEventId($eventId)
+    {
+        $result = ApiClient::get(ApiEndpoints::EVENT_COMPETITION_LIST, array('event_id' => $eventId));
+        if ($result['success'] && isset($result['data']['data'])) {
+            return $result['data']['data'];
+        }
+        return array();
+    }
+
+    public static function storeViaApi($eventId, $competitionId)
+    {
+        return ApiClient::post(ApiEndpoints::EVENT_COMPETITION_STORE, array(
+            'event_id' => $eventId,
+            'competition_id' => $competitionId,
+        ));
+    }
+
+    public static function deleteViaApi($id)
+    {
+        $url = ApiEndpoints::url(ApiEndpoints::EVENT_COMPETITION_DESTROY, array('id' => $id));
+        return ApiClient::delete($url);
+    }
 }
