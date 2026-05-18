94d7e0081e55091fea8693af0964147c32308480
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-18 16:40:37 +0700

    Auto-commit: 2026-05-18_16:40:37

--- protected/modules/admin/views/registrations/view.php
+++ protected/modules/admin/views/registrations/view.php
@@ -591,7 +591,7 @@ $contentConfig = array(
                         </div>
                         <div class="col-md-3">
                             <label class="form-label">Ngày đi <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control datepicker" name="departure_date" id="staff_departure_date" required placeholder="dd/mm/yyyy" autocomplete="off">
+                            <input type="text" class="form-control datepicker" name="check_out_date" id="staff_departure_date" required placeholder="dd/mm/yyyy" autocomplete="off">
                         </div>
                         <div class="col-md-3">
                             <label class="form-label">Phương tiện <span class="text-danger">*</span></label>
@@ -718,7 +718,7 @@ $contentConfig = array(
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Ngày đi</label>
-                                        <input type="text" class="form-control datepicker" name="departure_date" id="edit_departure_date" placeholder="dd/mm/yyyy" autocomplete="off">
+                                        <input type="text" class="form-control datepicker" name="check_out_date" id="edit_departure_date" placeholder="dd/mm/yyyy" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
@@ -824,7 +824,7 @@ $contentConfig = array(
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Ngày đi <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control datepicker" name="departure_date" id="add_departure_date" required placeholder="dd/mm/yyyy" autocomplete="off">
+                                        <input type="text" class="form-control datepicker" name="check_out_date" id="add_departure_date" required placeholder="dd/mm/yyyy" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
