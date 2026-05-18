81bf869e955f2e35f40a116d1cdf7f62b8d73947
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-18 13:16:01 +0700

    Auto-commit: 2026-05-18_13:16:01

--- protected/models/Departments.php
+++ protected/models/Departments.php
@@ -7,4 +7,21 @@ class Departments extends BaseDepartments
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
+
+	public static function findByPropertyAndDivision($propertyCode, $divisionCode)
+	{
+		if (empty($propertyCode) || empty($divisionCode)) {
+			return null;
+		}
+		return self::model()->find('property_code=:pc AND division_code=:dc AND deleted_at IS NULL', array(
+			':pc' => $propertyCode,
+			':dc' => $divisionCode,
+		));
+	}
+
+	public static function getDepartmentName($propertyCode, $divisionCode)
+	{
+		$dept = self::findByPropertyAndDivision($propertyCode, $divisionCode);
+		return $dept ? $dept->name : '';
+	}
 }
