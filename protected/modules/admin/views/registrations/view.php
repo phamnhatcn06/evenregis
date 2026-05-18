4789ed9c96b7f195e428be4502a095edd7cfe507
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-18 16:41:07 +0700

    Auto-commit: 2026-05-18_16:41:07

--- protected/modules/admin/views/registrations/view.php
+++ protected/modules/admin/views/registrations/view.php
@@ -818,7 +818,7 @@ $contentConfig = array(
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Ngày đến <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control datepicker" name="check_in_date" id="add_arrival_date" required placeholder="dd/mm/yyyy" autocomplete="off">
+                                        <input type="text" class="form-control datepicker" name="check_in_date" id="add_check_in_date" required placeholder="dd/mm/yyyy" autocomplete="off">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
