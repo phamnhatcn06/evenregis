754ed586d4f4d880df1e24afe466b578dde7c5fe
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-19 13:24:50 +0700

    Auto-commit: 2026-05-19_13:24:50

---
+++ protected/modules/admin/views/registrations/_modal_all_documents.php
@@ -0,0 +1,14 @@
+<!-- Modal View All Documents -->
+<div class="modal fade" id="allDocumentsModal" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tài liệu đính kèm</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="all_documents_viewer">
+                <!-- Content will be injected dynamically -->
+            </div>
+        </div>
+    </div>
+</div>
