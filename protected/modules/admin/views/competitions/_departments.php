f2ffb1aa02fe1506a69689ec8b775cc9fbb3107b
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-21 10:55:52 +0700

    Auto-commit: 2026-05-21_10:55:52

--- protected/modules/admin/views/competitions/_departments.php
+++ protected/modules/admin/views/competitions/_departments.php
@@ -68,23 +68,3 @@ Yii::app()->clientScript->registerScriptFile(
         <?php endif; ?>
     </div>
 </div>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var selectAllBtn = document.getElementById('select-all-depts');
-    var deselectAllBtn = document.getElementById('deselect-all-depts');
-    var checkboxes = document.querySelectorAll('.department-checkbox');
-
-    if (selectAllBtn) {
-        selectAllBtn.addEventListener('click', function() {
-            checkboxes.forEach(function(cb) { cb.checked = true; });
-        });
-    }
-
-    if (deselectAllBtn) {
-        deselectAllBtn.addEventListener('click', function() {
-            checkboxes.forEach(function(cb) { cb.checked = false; });
-        });
-    }
-});
-</script>
