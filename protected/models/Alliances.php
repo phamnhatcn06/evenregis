9e2614a4e24db3c10757c66dac7797641d93725b
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-22 09:24:38 +0700

    Auto-commit: 2026-05-22_09:24:38

--- protected/models/Alliances.php
+++ protected/models/Alliances.php
@@ -4,6 +4,9 @@ Yii::import('application.models._base.BaseAlliances');
 
 class Alliances extends BaseAlliances
 {
+	// Virtual property từ API
+	public $event_content_id;
+
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
