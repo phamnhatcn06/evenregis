e45e9d31fe5da289f69aa2ce46e5e93195f18b11
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-22 10:02:30 +0700

    Auto-commit: 2026-05-22_10:02:30

--- protected/models/_base/BaseContents.php
+++ protected/models/_base/BaseContents.php
@@ -15,6 +15,7 @@
  * @property string $description
  * @property integer $status
  * @property integer $sort_order
+ * @property integer $max_alliance_teams
  * @property string $created_at
  * @property string $updated_at
  * @property string $deleted_at
