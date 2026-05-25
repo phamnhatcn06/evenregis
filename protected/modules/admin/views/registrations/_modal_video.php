5bd539aca7a3c00e4f41573e095769e94662a2cf
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-25 12:43:14 +0700

    Auto-commit: 2026-05-25_12:43:14

---
+++ protected/modules/admin/views/registrations/_modal_video.php
@@ -0,0 +1,22 @@
+<!-- Modal Xem Video Tiết Mục Văn Nghệ -->
+<div class="modal fade" id="videoModal" tabindex="-1">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fa fa-play-circle me-2"></i><span id="videoModalTitle">Video tiết mục</span></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" onclick="stopVideo()"></button>
+            </div>
+            <div class="modal-body p-0">
+                <div id="videoContainer" class="ratio ratio-16x9">
+                    <!-- Video content will be injected here -->
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="#" id="videoDownloadLink" class="btn btn-outline-primary" target="_blank" style="display:none;">
+                    <i class="fa fa-external-link me-1"></i>Mở link gốc
+                </a>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="stopVideo()">Đóng</button>
+            </div>
+        </div>
+    </div>
+</div>
