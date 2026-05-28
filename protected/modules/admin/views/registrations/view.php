32a8fcbcb68f2bb799986bf19b736a30848e11b8
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-28 13:37:59 +0700

    Auto-commit: 2026-05-28_13:37:59

--- protected/modules/admin/views/registrations/view.php
+++ protected/modules/admin/views/registrations/view.php
@@ -1,3 +1,10 @@
+<style>
+.btn-outline-danger-hover:hover {
+    background-color: #dc3545 !important;
+    color: #fff !important;
+    border-color: #dc3545 !important;
+}
+</style>
 <?php
 $canEdit = in_array($model->status, array(Registrations::STATUS_DRAFT, Registrations::STATUS_REJECTED));
 
