113ab1ccc21f4856eff7ab42da64b7de5b8c7e29
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-28 15:02:22 +0700

    Auto-commit: 2026-05-28_15:02:22

--- protected/modules/admin/views/registrations/_modal_all_documents.php
+++ protected/modules/admin/views/registrations/_modal_all_documents.php
@@ -1,6 +1,6 @@
 <!-- Modal View All Documents -->
 <div class="modal fade" id="allDocumentsModal" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Tài liệu đính kèm</h5>
