db7a6ba76d26522667d2c5ddadf0878bbd751f9d
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-28 09:53:29 +0700

    Auto-commit: 2026-05-28_09:53:29

--- protected/models/RegistrationApprovalLogs.php
+++ protected/models/RegistrationApprovalLogs.php
@@ -9,35 +9,34 @@ class RegistrationApprovalLogs extends BaseRegistrationApprovalLogs
         return parent::model($className);
     }
 
-    /**
-     * Ghi log
-     */
-    public static function log($registrationId, $stepIndex, $action, $portalUserId = null, $comment = null, $stepName = null, $approverName = null, $returnToIndex = null)
-    {
-        $log = new self;
-        $log->registration_id = $registrationId;
-        $log->step_index = $stepIndex;
-        $log->step_name = $stepName;
-        $log->action = $action;
-        $log->return_to_index = $returnToIndex;
-        $log->approver_portal_id = $portalUserId;
-        $log->approver_name = $approverName;
-        $log->comment = $comment;
-        $log->acted_at = time();
+    // ==================== API Methods ====================
 
-        return $log->save();
+    public static function getApiDataProvider($params = array(), $pageSize = 25)
+    {
+        return new ApiDataProvider(ApiEndpoints::REGISTRATION_APPROVAL_LOG_LIST, array(
+            'modelClass' => 'RegistrationApprovalLogs',
+            'params' => $params,
+            'pagination' => array('pageSize' => $pageSize),
+        ));
     }
 
     /**
-     * Lấy lịch sử của một đăng ký
+     * Lấy lịch sử của một đăng ký - gọi API
      */
     public static function getHistory($registrationId)
     {
-        return self::model()->findAll(array(
-            'condition' => 'registration_id = :rid',
-            'params' => array(':rid' => $registrationId),
-            'order' => 'acted_at DESC',
-        ));
+        $url = ApiEndpoints::url(ApiEndpoints::REGISTRATION_APPROVAL_LOG_BY_REGISTRATION, array('registration_id' => $registrationId));
+        $result = ApiClient::get($url);
+
+        $models = array();
+        if ($result['success'] && !empty($result['data']['data'])) {
+            foreach ($result['data']['data'] as $item) {
+                $model = new self;
+                $model->setAttributes($item, false);
+                $models[] = $model;
+            }
+        }
+        return $models;
     }
 
     /**
@@ -46,5 +45,20 @@ class RegistrationApprovalLogs extends BaseRegistrationApprovalLogs
     public function getActedAtFormatted()
     {
         return date('d/m/Y H:i', $this->acted_at);
+    }
+
+    /**
+     * Lấy tên action hiển thị
+     */
+    public function getActionText()
+    {
+        $texts = array(
+            self::ACTION_APPROVED => 'Đã duyệt',
+            self::ACTION_REJECTED => 'Từ chối',
+            self::ACTION_REVISION => 'Yêu cầu chỉnh sửa',
+            self::ACTION_SUBMITTED => 'Nộp đăng ký',
+            self::ACTION_RESUBMITTED => 'Nộp lại',
+        );
+        return isset($texts[$this->action]) ? $texts[$this->action] : $this->action;
     }
 }
