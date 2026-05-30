bec6c4e74849ced612626a455feea446d3dd95ce
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-05-30 11:16:00 +0700

    Auto-commit: 2026-05-30_11:16:00

--- protected/config/params.php
+++ protected/config/params.php
@@ -15,6 +15,7 @@ return array(
         'sso_me_endpoint' => '/api/sso/me',
         'jwt_secret' => getenv('JWT_SECRET') ?: 'B7BFCA89BF11459E898B26310C2794E6819FB0AD0565B4C3',
         'jwt_algorithm' => 'HS256',
+        'portal_secret' => getenv('PORTAL_SECRET') ?: 'YOUR_PORTAL_SECRET_HERE',
     ),
 
     // External API
