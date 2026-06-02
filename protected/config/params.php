a4ce445204de29091374c0a42277404a4beb4e5e
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-02 13:19:12 +0700

    cọmmit

--- protected/config/params.php
+++ protected/config/params.php
@@ -10,8 +10,8 @@ return array(
 
     // Portal SSO Configuration
     'portal' => array(
-        'url' => 'http://localhost:5993/',
-        'api_url' => 'http://localhost:6689',
+        'url' => 'https://portal.muongthanh.vn',
+        'api_url' => 'https://api.portal.muongthanh.vn',
         'sso_me_endpoint' => '/api/sso/me',
         'sso_permissions_endpoint' => '/api/sso/permissions/me',
         'jwt_secret' => getenv('JWT_SECRET') ?: 'B7BFCA89BF11459E898B26310C2794E6819FB0AD0565B4C3',
@@ -20,8 +20,8 @@ return array(
     ),
 
     // External API
-    'externalApiUrl' => 'https://dev-portal-registration.muongthanh.vn',
-    'externalApiKey' => 'z8H4VaRvLtBGYUuLgasJDIJIWXYgquFgUIy426pkaFKi7Q0PAC44oz2Jy4KLB5Mz',
+    'externalApiUrl' => 'https://portal-registration.muongthanh.vn',
+    'externalApiKey' => 'XkufRQtY0evNxRyG0YvEXsq5Vg8hypjWDEaYqJlfTsc9qv8r8gYT9XqOlB8YLoQp',
 
     // Session Configuration
     'session' => array(
