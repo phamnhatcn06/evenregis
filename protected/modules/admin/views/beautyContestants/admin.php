ea153de725161f7abc5e3de3c6f362896143890f
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-05 11:52:24 +0700

    Auto-commit: 2026-06-05_11:52:24

--- protected/modules/admin/views/beautyContestants/admin.php
+++ protected/modules/admin/views/beautyContestants/admin.php
@@ -26,7 +26,6 @@ $this->Tabletitle = 'Danh sách thí sinh thi Miss';
             'filter' => true,
             'columns' => array(
                 array('name' => 'id', 'header' => 'ID', 'width' => '60px', 'filter' => false),
-                array('name' => 'contestant_number', 'header' => 'SBD', 'width' => '80px'),
                 array(
                     'name' => 'contest_id',
                     'header' => 'Cuộc thi',
@@ -36,42 +35,18 @@ $this->Tabletitle = 'Danh sách thí sinh thi Miss';
                         return isset($data->contest_name) ? CHtml::encode($data->contest_name) : $data->contest_id;
                     }
                 ),
+                array('name' => 'candidate_number', 'header' => 'SBD', 'width' => '80px'),
                 array(
                     'name' => 'attendee_id',
                     'header' => 'Thí sinh',
                     'type' => 'raw',
                     'value' => function ($data) {
-                        if (isset($data->attendee) && isset($data->attendee['full_name'])) {
-                            return CHtml::encode($data->attendee['full_name']);
+                        if (isset($data->members) && !empty($data->members)) {
+                            return CHtml::encode($data->members[0]['attendee_name']);
                         }
-                        return isset($data->attendee_name) ? CHtml::encode($data->attendee_name) : $data->attendee_id;
+                        return $data->attendee_id;
                     }
                 ),
-                array(
-                    'header' => 'Mã cụm',
-                    'type' => 'raw',
-                    'width' => '80px',
-                    'filter' => false,
-                    'value' => function ($data) {
-                        if (isset($data->attendee['property']['regional']['code'])) {
-                            return CHtml::encode($data->attendee['property']['regional']['code']);
-                        }
-                        return '';
-                    }
-                ),
-                array(
-                    'header' => 'Đơn vị',
-                    'type' => 'raw',
-                    'filter' => false,
-                    'value' => function ($data) {
-                        if (isset($data->attendee) && isset($data->attendee['property']) && isset($data->attendee['property']['name'])) {
-                            return CHtml::encode($data->attendee['property']['name']);
-                        }
-                        return isset($data->property_name) ? CHtml::encode($data->property_name) : '';
-                    }
-                ),
-                array('name' => 'height_cm', 'header' => 'Cao (cm)', 'width' => '80px', 'filter' => false),
-                array('name' => 'weight_kg', 'header' => 'Nặng (kg)', 'width' => '80px', 'filter' => false),
                 array(
                     'name' => 'status',
                     'header' => 'Trạng thái',
@@ -80,16 +55,6 @@ $this->Tabletitle = 'Danh sách thí sinh thi Miss';
                     'filter' => BeautyContestants::getStatusOptions(),
                     'value' => function ($data) {
                         return BeautyContestants::getStatusLabel($data->status);
-                    }
-                ),
-                array(
-                    'header' => 'Thao tác',
-                    'width' => '100px',
-                    'type' => 'raw',
-                    'filter' => false,
-                    'sortable' => false,
-                    'value' => function ($data) {
-                        return IconHelper::actionButtons($data, array('view', 'update', 'delete'), '/admin/beautyContestants');
                     }
                 ),
             ),
