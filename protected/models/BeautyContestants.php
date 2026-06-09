1d73ad745ba9d422135527fa584a619ad4a14a0e
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-09 15:44:00 +0700

    Auto-commit: 2026-06-09_15:44:00

--- protected/models/BeautyContestants.php
+++ protected/models/BeautyContestants.php
@@ -108,7 +108,7 @@ class BeautyContestants extends BaseBeautyContestants
         return ApiClient::delete($url);
     }
 
-    public static function getApiDataProvider($params = array(), $pageSize = 25)
+    public static function getApiDataProvider($params = array(), $pageSize = 10000)
     {
         return new ApiDataProvider(ApiEndpoints::BEAUTY_CONTESTANT_LIST, array(
             'modelClass' => 'BeautyContestants',
