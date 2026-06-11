5931aaa944b35275b6f36d924f8f4293c9064454
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-11 16:54:47 +0700

    Auto-commit: 2026-06-11_16:54:47

---
+++ protected/modules/admin/views/competitionRegistrations/view_by_competition.php
@@ -0,0 +1,26 @@
+<?php
+$this->breadcrumbs = array(
+    'Thi nghiệp vụ' => array('admin'),
+    'Xem theo nghiệp vụ',
+);
+
+$this->menu = array(
+    array(
+        'label' => 'Tổng quan',
+        'url' => $this->createUrl('overview'),
+        'color' => 'secondary',
+        'icon' => 'fa-arrow-left',
+    ),
+);
+$this->Tabletitle = 'Thí sinh thi nghiệp vụ: ' . CHtml::encode($competitionName);
+?>
+
+<?php $this->renderPartial('_view_by_competition', array(
+    'competitionName' => $competitionName,
+    'eventName' => $eventName,
+    'eventId' => $eventId,
+    'competitionId' => $competitionId,
+    'contestantsByRegion' => $contestantsByRegion,
+    'regionList' => $regionList,
+    'competitionsList' => $competitionsList,
+)); ?>
