d14cdbaed05ae8f0b41de1f58ddf72559facbd9b
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-11 16:54:28 +0700

    Auto-commit: 2026-06-11_16:54:28

--- protected/modules/admin/controllers/CompetitionRegistrationsController.php
+++ protected/modules/admin/controllers/CompetitionRegistrationsController.php
@@ -129,6 +129,265 @@ class CompetitionRegistrationsController extends AdminController
         }
     }
 
+    public function actionOverview()
+    {
+        $events = Events::getActiveList();
+        $competitions = Competitions::getActiveList();
+
+        $this->render('overview', array(
+            'events' => $events,
+            'competitions' => $competitions,
+        ));
+    }
+
+    public function actionGetOverviewStats()
+    {
+        $eventId = Yii::app()->request->getQuery('event_id');
+        if (empty($eventId)) {
+            $activeEvents = Events::getActiveList();
+            if (!empty($activeEvents)) {
+                $eventId = key($activeEvents);
+            }
+        }
+
+        if (empty($eventId)) {
+            header('Content-Type: application/json');
+            echo json_encode(array(
+                'success' => true,
+                'total_contestants' => 0,
+                'competitions' => array(),
+            ));
+            Yii::app()->end();
+        }
+
+        $registrationsRes = Registrations::getApiDataProvider(array(
+            'event_id' => $eventId,
+            'per_page' => 1000,
+        ), 1000)->getData();
+
+        $activeRegsMap = array();
+        foreach ($registrationsRes as $reg) {
+            if (isset($reg->deleted_at) && $reg->deleted_at !== null && $reg->deleted_at !== '') {
+                continue;
+            }
+            $activeRegsMap[$reg->id] = true;
+        }
+
+        $compRegsRes = CompetitionRegistrations::getApiDataProvider(array(
+            'event_id' => $eventId,
+            'per_page' => 5000,
+        ), 5000)->getData();
+
+        $competitionStats = array();
+        $totalContestants = 0;
+        $confirmedCount = 0;
+        $pendingCount = 0;
+
+        foreach ($compRegsRes as $compReg) {
+            if (isset($compReg->deleted_at) && $compReg->deleted_at !== null && $compReg->deleted_at !== '') {
+                continue;
+            }
+
+            $compId = $compReg->competition_id;
+            $compName = isset($compReg->competition) ? $compReg->competition->name : (isset($compReg->competition_name) ? $compReg->competition_name : 'Chưa xác định');
+
+            if (!isset($competitionStats[$compId])) {
+                $competitionStats[$compId] = array(
+                    'id' => $compId,
+                    'name' => $compName,
+                    'contestant_count' => 0,
+                    'confirmed_count' => 0,
+                    'pending_count' => 0,
+                );
+            }
+
+            $competitionStats[$compId]['contestant_count']++;
+            $totalContestants++;
+
+            if ($compReg->status == CompetitionRegistrations::STATUS_CONFIRMED) {
+                $competitionStats[$compId]['confirmed_count']++;
+                $confirmedCount++;
+            } else if ($compReg->status == CompetitionRegistrations::STATUS_PENDING) {
+                $competitionStats[$compId]['pending_count']++;
+                $pendingCount++;
+            }
+        }
+
+        $formattedCompetitions = array_values($competitionStats);
+        usort($formattedCompetitions, function ($a, $b) {
+            return strnatcasecmp($a['name'], $b['name']);
+        });
+
+        header('Content-Type: application/json');
+        echo json_encode(array(
+            'success' => true,
+            'total_contestants' => $totalContestants,
+            'confirmed_count' => $confirmedCount,
+            'pending_count' => $pendingCount,
+            'competitions' => $formattedCompetitions,
+        ));
+        Yii::app()->end();
+    }
+
+    public function actionViewByCompetition()
+    {
+        $eventId = Yii::app()->request->getQuery('event_id');
+        $competitionId = Yii::app()->request->getQuery('competition_id');
+
+        $compRegs = CompetitionRegistrations::getApiDataProvider(array(
+            'event_id' => $eventId,
+            'competition_id' => $competitionId,
+            'per_page' => 5000,
+        ), 5000)->getData();
+
+        $regionals = Regionals::getApiDataProvider(array(), 100)->getData();
+        $regionalMap = array();
+        $regionalCodeMap = array();
+        foreach ($regionals as $r) {
+            $regionalMap[$r->id] = $r->name;
+            $regionalCodeMap[$r->id] = isset($r->code) ? $r->code : '';
+        }
+
+        $properties = Properties::getApiDataProvider(array(), 500)->getData();
+        $propertyRegionMap = array();
+        $propertyNameMap = array();
+        foreach ($properties as $p) {
+            $propertyRegionMap[$p->id] = isset($p->region_id) ? $p->region_id : null;
+            $propertyNameMap[$p->id] = $p->name;
+        }
+
+        $contestantsByRegion = array();
+        foreach ($compRegs as $compReg) {
+            if (isset($compReg->deleted_at) && $compReg->deleted_at !== null && $compReg->deleted_at !== '') {
+                continue;
+            }
+
+            $propId = isset($compReg->attendee) && isset($compReg->attendee->property_id)
+                ? $compReg->attendee->property_id
+                : (isset($compReg->property_id) ? $compReg->property_id : null);
+            $propName = isset($propertyNameMap[$propId]) ? $propertyNameMap[$propId] : (isset($compReg->property_name) ? $compReg->property_name : 'Chưa xác định');
+
+            $regionId = isset($propertyRegionMap[$propId]) ? $propertyRegionMap[$propId] : null;
+            $regionName = ($regionId && isset($regionalMap[$regionId])) ? $regionalMap[$regionId] : 'Chưa phân cụm';
+            $regionCode = ($regionId && isset($regionalCodeMap[$regionId])) ? $regionalCodeMap[$regionId] : 'ZZZ';
+
+            if (!isset($contestantsByRegion[$regionId])) {
+                $contestantsByRegion[$regionId] = array(
+                    'region_id' => $regionId,
+                    'region_name' => $regionName,
+                    'region_code' => $regionCode,
+                    'properties' => array(),
+                );
+            }
+
+            if (!isset($contestantsByRegion[$regionId]['properties'][$propId])) {
+                $contestantsByRegion[$regionId]['properties'][$propId] = array(
+                    'property_name' => $propName,
+                    'contestants' => array(),
+                );
+            }
+
+            $attendeeName = isset($compReg->attendee) ? $compReg->attendee->full_name : (isset($compReg->attendee_name) ? $compReg->attendee_name : '-');
+            $attendeePosition = isset($compReg->attendee) ? $compReg->attendee->position : (isset($compReg->attendee_position) ? $compReg->attendee_position : '');
+            $attendeeGender = isset($compReg->attendee) ? $compReg->attendee->gender : (isset($compReg->attendee_gender) ? $compReg->attendee_gender : '');
+
+            $contestantsByRegion[$regionId]['properties'][$propId]['contestants'][] = array(
+                'id' => $compReg->id,
+                'attendee_id' => $compReg->attendee_id,
+                'candidate_number' => $compReg->candidate_number,
+                'attendee_name' => $attendeeName,
+                'attendee_position' => $attendeePosition,
+                'attendee_gender' => $attendeeGender,
+                'status' => $compReg->status,
+                'registered_at' => $compReg->registered_at,
+                'note' => $compReg->note,
+            );
+        }
+
+        foreach ($contestantsByRegion as &$region) {
+            $region['properties'] = array_values($region['properties']);
+        }
+        unset($region);
+
+        $eventName = '';
+        $competitionName = '';
+        $eventList = Events::getActiveList();
+        if (isset($eventList[$eventId])) {
+            $eventName = $eventList[$eventId];
+        }
+        $competition = Competitions::fetchFromApi($competitionId);
+        if ($competition) {
+            $competitionName = $competition->name;
+        }
+
+        $competitionsList = array();
+        $activeComps = Competitions::getApiDataProvider(array('is_active' => 1), 100)->getData();
+        foreach ($activeComps as $comp) {
+            $competitionsList[] = array(
+                'id' => $comp->id,
+                'name' => $comp->name,
+            );
+        }
+        usort($competitionsList, function ($a, $b) {
+            return strnatcasecmp($a['name'], $b['name']);
+        });
+
+        uasort($contestantsByRegion, function ($a, $b) {
+            return strcmp($a['region_code'], $b['region_code']);
+        });
+
+        $regionList = array();
+        foreach ($contestantsByRegion as $regionData) {
+            $regionList[$regionData['region_id']] = $regionData['region_name'];
+        }
+
+        $this->render('view_by_competition', array(
+            'competitionName' => $competitionName,
+            'eventName' => $eventName,
+            'eventId' => $eventId,
+            'competitionId' => $competitionId,
+            'contestantsByRegion' => array_values($contestantsByRegion),
+            'regionList' => $regionList,
+            'competitionsList' => $competitionsList,
+        ));
+    }
+
+    public function actionAjaxView()
+    {
+        $id = Yii::app()->request->getQuery('id');
+        header('Content-Type: application/json');
+
+        $model = CompetitionRegistrations::fetchFromApi($id);
+        if (!$model) {
+            echo json_encode(array('success' => false, 'message' => 'Không tìm thấy'));
+            Yii::app()->end();
+        }
+
+        $attendeeName = isset($model->attendee) ? $model->attendee->full_name : (isset($model->attendee_name) ? $model->attendee_name : '-');
+        $attendeePosition = isset($model->attendee) ? $model->attendee->position : (isset($model->attendee_position) ? $model->attendee_position : '');
+        $attendeeGender = isset($model->attendee) ? $model->attendee->gender : (isset($model->attendee_gender) ? $model->attendee_gender : '');
+        $propertyName = isset($model->property_name) ? $model->property_name : (isset($model->attendee) && isset($model->attendee->property_name) ? $model->attendee->property_name : '');
+        $competitionName = isset($model->competition) ? $model->competition->name : (isset($model->competition_name) ? $model->competition_name : '');
+
+        echo json_encode(array(
+            'success' => true,
+            'data' => array(
+                'id' => $model->id,
+                'candidate_number' => $model->candidate_number,
+                'attendee_name' => $attendeeName,
+                'attendee_position' => $attendeePosition,
+                'attendee_gender' => $attendeeGender,
+                'property_name' => $propertyName,
+                'competition_name' => $competitionName,
+                'status' => $model->status,
+                'status_label' => CompetitionRegistrations::getStatusLabel($model->status),
+                'registered_at' => MyHelper::formatDateTime($model->registered_at),
+                'note' => $model->note,
+            ),
+        ));
+        Yii::app()->end();
+    }
+
     protected function loadModelById($id)
     {
         $model = CompetitionRegistrations::fetchFromApi($id);
