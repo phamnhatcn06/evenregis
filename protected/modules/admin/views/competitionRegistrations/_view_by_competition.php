9e8387f287af0a79084a26519645fda619360c49
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-12 10:47:10 +0700

    Auto-commit: 2026-06-12_10:47:10

--- protected/modules/admin/views/competitionRegistrations/_view_by_competition.php
+++ protected/modules/admin/views/competitionRegistrations/_view_by_competition.php
@@ -169,7 +169,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header bg-primary text-white">
-                <h5 class="modal-title" id="modalViewContestantLabel">Chi tiết thí sinh</h5>
+                <h5 class="modal-title text-white" id="modalViewContestantLabel">Chi tiết thí sinh</h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Đóng"></button>
             </div>
             <div class="modal-body" id="modalViewContestantBody">
