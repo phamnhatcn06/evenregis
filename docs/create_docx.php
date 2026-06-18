aa9b4899b163ee03775fa12bf34429d91addc4de
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-18 13:42:53 +0700

    Auto-commit: 2026-06-18_13:42:53

--- docs/create_docx.php
+++ docs/create_docx.php
@@ -1,6 +1,4 @@
 <?php
-// Simple DOCX generator
-
 $content = <<<'XML'
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
@@ -10,65 +8,57 @@ $content = <<<'XML'
 <w:r><w:rPr><w:b/><w:sz w:val="48"/></w:rPr><w:t>LỊCH THI ĐẤU BÓNG BÀN</w:t></w:r></w:p>
 
 <w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Thông tin chung:</w:t></w:r></w:p>
-<w:p><w:r><w:t>• Thời gian: 2 ngày</w:t></w:r></w:p>
-<w:p><w:r><w:t>• Ngày 1: Chiều 13:30 - 17:30</w:t></w:r></w:p>
-<w:p><w:r><w:t>• Ngày 2: Sáng 7:30 - 11:30, Chiều 13:00 - 18:00</w:t></w:r></w:p>
+<w:p><w:r><w:t>• Số bàn thi đấu: 2 bàn</w:t></w:r></w:p>
 <w:p><w:r><w:t>• Thời lượng mỗi trận: 30 phút</w:t></w:r></w:p>
+<w:p><w:r><w:t>• Ngày 1: Chiều 13:30 - 17:30</w:t></w:r></w:p>
+<w:p><w:r><w:t>• Ngày 2: Sáng 7:30 - 11:30, Chiều 14:00 - 16:30</w:t></w:r></w:p>
 
 <w:p><w:r><w:t></w:t></w:r></w:p>
 <w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>NGÀY 1 - CHIỀU (13:30 - 17:30)</w:t></w:r></w:p>
 
 <w:tbl>
-<w:tblPr><w:tblW w:w="9000" w:type="dxa"/><w:tblBorders>
+<w:tblPr><w:tblW w:w="9500" w:type="dxa"/><w:tblBorders>
 <w:top w:val="single" w:sz="4"/><w:left w:val="single" w:sz="4"/><w:bottom w:val="single" w:sz="4"/><w:right w:val="single" w:sz="4"/><w:insideH w:val="single" w:sz="4"/><w:insideV w:val="single" w:sz="4"/>
 </w:tblBorders></w:tblPr>
-<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>STT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Nội dung</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Vòng đấu</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Trận</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>1</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>13:30-14:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>ĐN1 vs ĐN8</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>2</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>14:00-14:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>DNữ1 vs DNữ8</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>3</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>14:30-15:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>ĐN2 vs ĐN7</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>4</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>15:00-15:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>DNữ2 vs DNữ7</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>5</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>15:30-16:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>ĐN3 vs ĐN6</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>6</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>16:00-16:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>DNữ3 vs DNữ6</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>7</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>16:30-17:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>ĐN4 vs ĐN5</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>8</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>17:00-17:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Tứ kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>DNữ4 vs DNữ5</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Bàn 1</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Bàn 2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>13:30-14:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam TK: ĐN1 vs ĐN8</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ TK: DNữ1 vs DNữ8</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>14:00-14:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam TK: ĐN2 vs ĐN7</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ TK: DNữ2 vs DNữ7</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>14:30-15:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam TK: ĐN3 vs ĐN6</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ TK: DNữ3 vs DNữ6</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>15:00-15:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam TK: ĐN4 vs ĐN5</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ TK: DNữ4 vs DNữ5</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>15:30-16:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>16:00-16:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A1 vs A2</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng B: B1 vs B2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>16:30-17:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A3 vs A4</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng B: B1 vs B3</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>17:00-17:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A1 vs A3</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng B: B2 vs B3</w:t></w:r></w:p></w:tc></w:tr>
 </w:tbl>
 
 <w:p><w:r><w:t></w:t></w:r></w:p>
-<w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>NGÀY 2 - SÁNG (7:30 - 11:30)</w:t></w:r></w:p>
+<w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>NGÀY 2 - SÁNG (7:30 - 11:00)</w:t></w:r></w:p>
 
 <w:tbl>
-<w:tblPr><w:tblW w:w="9000" w:type="dxa"/><w:tblBorders>
+<w:tblPr><w:tblW w:w="9500" w:type="dxa"/><w:tblBorders>
 <w:top w:val="single" w:sz="4"/><w:left w:val="single" w:sz="4"/><w:bottom w:val="single" w:sz="4"/><w:right w:val="single" w:sz="4"/><w:insideH w:val="single" w:sz="4"/><w:insideV w:val="single" w:sz="4"/>
 </w:tblBorders></w:tblPr>
-<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>STT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Nội dung</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Vòng đấu</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Trận</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>9</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>07:30-08:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A1 vs A2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>10</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>08:00-08:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng B</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>B1 vs B2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>11</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>08:30-09:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A3 vs A4</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>12</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>09:00-09:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng B</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>B1 vs B3</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>13</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>09:30-10:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A1 vs A3</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>14</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>10:00-10:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng B</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>B2 vs B3</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>15</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>10:30-11:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A2 vs A4</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>16</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>11:00-11:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A1 vs A4</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Bàn 1</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Bàn 2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>07:30-08:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A2 vs A4</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A1 vs A4</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>08:00-08:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam Bảng A: A2 vs A3</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>---</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>08:30-09:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>09:00-09:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam BK: Thắng TK1 vs Thắng TK2</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ BK: Thắng TK1 vs Thắng TK2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>09:30-10:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam BK: Thắng TK3 vs Thắng TK4</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ BK: Thắng TK3 vs Thắng TK4</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>10:00-10:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:i/></w:rPr><w:t>--- Nghỉ giải lao ---</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>10:30-11:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam BK: Nhất A vs Nhì B</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam BK: Nhất B vs Nhì A</w:t></w:r></w:p></w:tc></w:tr>
 </w:tbl>
 
 <w:p><w:r><w:t></w:t></w:r></w:p>
-<w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>NGÀY 2 - CHIỀU (13:00 - 18:00)</w:t></w:r></w:p>
+<w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>NGÀY 2 - CHIỀU - CHUNG KẾT (14:00 - 16:30)</w:t></w:r></w:p>
 
 <w:tbl>
-<w:tblPr><w:tblW w:w="9000" w:type="dxa"/><w:tblBorders>
+<w:tblPr><w:tblW w:w="9500" w:type="dxa"/><w:tblBorders>
 <w:top w:val="single" w:sz="4"/><w:left w:val="single" w:sz="4"/><w:bottom w:val="single" w:sz="4"/><w:right w:val="single" w:sz="4"/><w:insideH w:val="single" w:sz="4"/><w:insideV w:val="single" w:sz="4"/>
 </w:tblBorders></w:tblPr>
-<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>STT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Nội dung</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Vòng đấu</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Trận</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>17</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>13:00-13:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bảng A</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>A2 vs A3</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>18</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>13:30-14:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng TK1 vs Thắng TK2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>19</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>14:00-14:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng TK1 vs Thắng TK2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>20</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>14:30-15:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng TK3 vs Thắng TK4</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>21</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>15:00-15:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng TK3 vs Thắng TK4</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>22</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>15:30-16:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Nhất A vs Nhì B</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>23</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>16:00-16:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Bán kết</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Nhất B vs Nhì A</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>24</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>16:30-17:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>25</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>17:00-17:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
-<w:tr><w:tc><w:p><w:r><w:t>26</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>17:30-18:00</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Giờ</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Nội dung</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:rPr><w:b/></w:rPr><w:t>Trận</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>14:00-14:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đôi Nam CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>15:00-15:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nữ CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
+<w:tr><w:tc><w:p><w:r><w:t>16:00-16:30</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Đơn Nam CHUNG KẾT</w:t></w:r></w:p></w:tc><w:tc><w:p><w:r><w:t>Thắng BK1 vs Thắng BK2</w:t></w:r></w:p></w:tc></w:tr>
 </w:tbl>
 
 <w:p><w:r><w:t></w:t></w:r></w:p>
@@ -78,11 +68,10 @@ $content = <<<'XML'
 
 <w:p><w:r><w:t></w:t></w:r></w:p>
 <w:p><w:r><w:rPr><w:b/><w:sz w:val="32"/></w:rPr><w:t>GHI CHÚ</w:t></w:r></w:p>
-<w:p><w:r><w:t>1. Ngày 1 chỉ thi đấu buổi chiều (tứ kết Đơn Nam và Đơn Nữ).</w:t></w:r></w:p>
-<w:p><w:r><w:t>2. Ngày 2 sáng: Vòng bảng Đôi Nam.</w:t></w:r></w:p>
-<w:p><w:r><w:t>3. Ngày 2 chiều: Bán kết + Chung kết cả 3 nội dung.</w:t></w:r></w:p>
-<w:p><w:r><w:t>4. Tránh trùng lặp: Đơn Nam xen kẽ Đơn Nữ để VĐV tham gia nhiều nội dung có thời gian nghỉ.</w:t></w:r></w:p>
-<w:p><w:r><w:t>5. Bán kết Đôi Nam: Nhất A vs Nhì B; Nhất B vs Nhì A.</w:t></w:r></w:p>
+<w:p><w:r><w:t>1. Thi đấu song song 2 bàn, Đơn Nam và Đơn Nữ cùng khung giờ (không trùng VĐV).</w:t></w:r></w:p>
+<w:p><w:r><w:t>2. Nghỉ 30 phút giữa Tứ kết và Vòng bảng để VĐV tham gia nhiều nội dung có thời gian nghỉ.</w:t></w:r></w:p>
+<w:p><w:r><w:t>3. Chung kết đánh riêng từng trận để khán giả theo dõi.</w:t></w:r></w:p>
+<w:p><w:r><w:t>4. Bán kết Đôi Nam: Nhất A vs Nhì B; Nhất B vs Nhì A.</w:t></w:r></w:p>
 
 </w:body>
 </w:document>
@@ -109,6 +98,4 @@ if ($zip->open($filename, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) 
     $zip->addFromString('word/document.xml', $content);
     $zip->close();
     echo "Created: $filename\n";
-} else {
-    echo "Failed to create DOCX\n";
 }
