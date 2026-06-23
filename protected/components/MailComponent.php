acf1ee36a0287eab2303e1f913ff4c39f7a91c06
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-24 02:17:37 +0700

    Auto-commit: 2026-06-24_02:17:37

---
+++ protected/components/MailComponent.php
@@ -0,0 +1,22 @@
+<?php
+
+Yii::import('ext.yii-mail.YiiMail');
+
+class MailComponent extends YiiMail
+{
+    public function init()
+    {
+        $mailConfig = Yii::app()->params['mail'];
+
+        $this->transportType = 'smtp';
+        $this->transportOptions = array(
+            'host' => $mailConfig['host'],
+            'username' => $mailConfig['username'],
+            'password' => $mailConfig['password'],
+            'port' => $mailConfig['port'],
+            'encryption' => $mailConfig['encryption'],
+        );
+
+        parent::init();
+    }
+}
