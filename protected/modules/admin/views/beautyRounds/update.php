799c5b7caecc99623f5950f4636e08cecd2273b3
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-25 14:13:10 +0700

    Auto-commit: 2026-06-25_14:13:10

---
+++ protected/modules/admin/views/beautyRounds/update.php
@@ -0,0 +1,30 @@
+<?php
+$this->breadcrumbs = array(
+    'Cuộc thi sắc đẹp' => array('/admin/beautyContests/admin'),
+    'Vòng thi' => array('admin'),
+    $model->name => array('view', 'id' => $model->id),
+    'Cập nhật',
+);
+
+$this->menu = array(
+    array(
+        'label' => 'Danh sách',
+        'url' => $this->createUrl('admin'),
+        'color' => 'secondary',
+        'icon' => 'fa-list',
+    ),
+    array(
+        'label' => 'Xem chi tiết',
+        'url' => $this->createUrl('view', array('id' => $model->id)),
+        'color' => 'info',
+        'icon' => 'fa-eye',
+    ),
+);
+$this->Tabletitle = 'Cập nhật: ' . CHtml::encode($model->name);
+?>
+
+<div class="card">
+    <div class="card-body">
+        <?php $this->renderPartial('_form', array('model' => $model, 'contests' => $contests)); ?>
+    </div>
+</div>
