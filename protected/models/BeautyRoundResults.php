250661ba4f97d744ab9872762c524e2acd114186
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-25 15:16:08 +0700

    Auto-commit: 2026-06-25_15:16:08

--- protected/models/BeautyRoundResults.php
+++ protected/models/BeautyRoundResults.php
@@ -86,12 +86,48 @@ class BeautyRoundResults extends BaseBeautyRoundResults
 
     public static function getAvailableContestants($roundId)
     {
-        $url = ApiEndpoints::url(ApiEndpoints::BEAUTY_ROUND_RESULT_AVAILABLE_CONTESTANTS, array('round_id' => $roundId));
-        $result = ApiClient::get($url);
-        if ($result['success'] && isset($result['data'])) {
-            return isset($result['data']['data']) ? $result['data']['data'] : $result['data'];
+        $round = BeautyRounds::fetchFromApi($roundId);
+        if (!$round || !$round->contest_id) {
+            return array();
         }
-        return array();
+
+        $contestantsResult = ApiClient::get(ApiEndpoints::BEAUTY_CONTESTANT_LIST, array(
+            'contest_id' => $round->contest_id,
+            'per_page' => 1000,
+        ));
+
+        if (!$contestantsResult['success'] || !isset($contestantsResult['data']['data'])) {
+            return array();
+        }
+
+        $allContestants = $contestantsResult['data']['data'];
+
+        $assignedResult = ApiClient::get(ApiEndpoints::BEAUTY_ROUND_RESULT_LIST, array(
+            'round_id' => $roundId,
+            'per_page' => 1000,
+        ));
+
+        $assignedIds = array();
+        if ($assignedResult['success'] && isset($assignedResult['data']['data'])) {
+            foreach ($assignedResult['data']['data'] as $r) {
+                $assignedIds[$r['registration_id']] = true;
+            }
+        }
+
+        $available = array();
+        foreach ($allContestants as $c) {
+            if (!isset($assignedIds[$c['id']])) {
+                $available[] = array(
+                    'registration_id' => $c['id'],
+                    'contestant_number' => isset($c['candidate_number']) ? $c['candidate_number'] : '',
+                    'contestant_name' => isset($c['attendee_name']) ? $c['attendee_name'] : '',
+                    'property_name' => isset($c['property_name']) ? $c['property_name'] : '',
+                    'photo_portrait' => isset($c['photo_portrait']) ? $c['photo_portrait'] : '',
+                );
+            }
+        }
+
+        return $available;
     }
 
     public static function assignContestants($roundId, $registrationIds)
