6aed23ed05fea7c8fc6d2a0439fa90a838105eb9
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-27 01:01:19 +0700

    Auto-commit: 2026-06-27_01:01:19

---
+++ protected/modules/admin/views/approveMiss/_modal_compare.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="modalCompare" tabindex="-1" aria-labelledby="modalCompareLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalCompareLabel">So sánh thí sinh</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row" id="compare_container">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+            </div>
+        </div>
+    </div>
+</div>
