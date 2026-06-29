b87c17d9fcef6d8a6a08f9bf742976462f0e8dd4
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-29 13:27:07 +0700

    Auto-commit: 2026-06-29_13:27:07

--- protected/modules/admin/views/approveMiss/_modal_compare.php
+++ protected/modules/admin/views/approveMiss/_modal_compare.php
@@ -16,6 +16,37 @@
     </div>
 </div>
 
+<div class="modal fade" id="modalApprove" tabindex="-1" aria-labelledby="modalApproveLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-success text-white">
+                <h5 class="modal-title" id="modalApproveLabel"><i class="fa fa-check me-2"></i>Duyệt thí sinh</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" id="approve_contestant_id">
+                <input type="hidden" id="approve_contest_id">
+                <p class="mb-3">Duyệt thí sinh: <strong id="approve_contestant_name"></strong></p>
+                <div class="mb-3">
+                    <label class="form-label fw-bold">Gán vào vòng thi:</label>
+                    <div id="rounds_loading" class="text-center py-3" style="display:none;">
+                        <i class="fa fa-spinner fa-spin fa-2x"></i>
+                    </div>
+                    <div id="rounds_list" class="list-group">
+                    </div>
+                    <small class="text-muted mt-2 d-block">Chọn vòng để gán thí sinh vào, hoặc bỏ qua để chỉ duyệt</small>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                <button type="button" class="btn btn-success" id="btn_confirm_approve">
+                    <i class="fa fa-check me-1"></i>Duyệt
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <template id="compare-card-template">
     <div class="compare-column">
         <div class="card h-100">
