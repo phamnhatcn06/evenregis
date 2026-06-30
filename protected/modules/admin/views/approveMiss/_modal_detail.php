283a36ee4e02b9aa8f60b3f8225d94d59cf221c8
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-06-30 09:14:50 +0700

    Auto-commit: 2026-06-30_09:14:50

--- protected/modules/admin/views/approveMiss/_modal_detail.php
+++ protected/modules/admin/views/approveMiss/_modal_detail.php
@@ -9,70 +9,88 @@
                 <input type="hidden" id="detail_id">
                 <input type="hidden" id="detail_contest_id">
 
-                <div class="row mb-4">
-                    <div class="col-12">
+                <div class="row h-100">
+                    <!-- Cột trái: 4 ảnh -->
+                    <div class="col-md-6">
+                        <div class="row g-3">
+                            <div class="col-6">
+                                <div class="text-center border rounded p-2">
+                                    <img id="detail_photo_portrait" src="" alt="Ảnh chân dung" class="img-fluid" style="height:280px;object-fit:contain;">
+                                    <div class="photo-label mt-2 fw-bold">Chân dung 1</div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-center border rounded p-2">
+                                    <img id="detail_photo_portrait_2" src="" alt="Ảnh chân dung 2" class="img-fluid" style="height:280px;object-fit:contain;">
+                                    <div class="photo-label mt-2 fw-bold">Chân dung 2</div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-center border rounded p-2">
+                                    <img id="detail_photo_full_body" src="" alt="Ảnh toàn thân" class="img-fluid" style="height:280px;object-fit:contain;">
+                                    <div class="photo-label mt-2 fw-bold">Toàn thân 1</div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-center border rounded p-2">
+                                    <img id="detail_photo_full_body_2" src="" alt="Ảnh toàn thân 2" class="img-fluid" style="height:280px;object-fit:contain;">
+                                    <div class="photo-label mt-2 fw-bold">Toàn thân 2</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Cột phải: Thông tin + Video -->
+                    <div class="col-md-6">
                         <table class="table table-bordered">
                             <tbody>
                                 <tr>
-                                    <th style="width:15%;">Họ tên</th>
+                                    <th style="width:35%;">Họ tên</th>
                                     <td id="detail_name"></td>
-                                    <th style="width:15%;">Đơn vị</th>
+                                </tr>
+                                <tr>
+                                    <th>Đơn vị</th>
                                     <td id="detail_property"></td>
                                 </tr>
                                 <tr>
                                     <th>Cuộc thi</th>
                                     <td id="detail_contest"></td>
+                                </tr>
+                                <tr>
                                     <th>Trạng thái</th>
                                     <td id="detail_status"></td>
                                 </tr>
                                 <tr>
                                     <th>Chiều cao</th>
                                     <td id="detail_height"></td>
+                                </tr>
+                                <tr>
                                     <th>Cân nặng</th>
                                     <td id="detail_weight"></td>
                                 </tr>
                                 <tr>
                                     <th>Số đo 3 vòng</th>
                                     <td id="detail_measurements"></td>
+                                </tr>
+                                <tr>
                                     <th>Năng khiếu</th>
                                     <td id="detail_talent"></td>
                                 </tr>
                                 <tr>
                                     <th>Email cá nhân</th>
                                     <td id="detail_email"></td>
+                                </tr>
+                                <tr>
                                     <th>Ngày gửi hồ sơ</th>
                                     <td id="detail_submitted_at"></td>
                                 </tr>
                             </tbody>
                         </table>
-                    </div>
-                </div>
 
-                <div class="row mb-4">
-                    <div class="col-md-4">
-                        <div class="photo-item-fullwidth text-center">
-                            <img id="detail_photo_portrait" src="" alt="Ảnh chân dung" class="img-fluid" style="max-height:500px;object-fit:contain;">
-                            <div class="photo-label mt-2">Chân dung</div>
+                        <div id="detail_video_container" class="mt-3" style="display:none;">
+                            <label class="form-label fw-bold">Video dự thi:</label>
+                            <video id="detail_video" controls class="w-100" style="max-height:300px;"></video>
                         </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="photo-item-fullwidth text-center">
-                            <img id="detail_photo_full_body" src="" alt="Ảnh toàn thân" class="img-fluid" style="max-height:500px;object-fit:contain;">
-                            <div class="photo-label mt-2">Toàn thân</div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="photo-item-fullwidth text-center">
-                            <img id="detail_photo_full_body_2" src="" alt="Ảnh toàn thân 2" class="img-fluid" style="max-height:500px;object-fit:contain;">
-                            <div class="photo-label mt-2">Toàn thân 2</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div id="detail_video_container" class="row" style="display:none;">
-                    <div class="col-12">
-                        <label class="form-label fw-bold">Video dự thi:</label>
-                        <video id="detail_video" controls class="w-100" style="max-height:400px;"></video>
                     </div>
                 </div>
             </div>
