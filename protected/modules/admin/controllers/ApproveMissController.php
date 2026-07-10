559165f53b87b1530bb12450361f0402c5cccb60
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-07-10 09:03:51 +0700

    Auto-commit: 2026-07-10_09:03:51

--- protected/modules/admin/controllers/ApproveMissController.php
+++ protected/modules/admin/controllers/ApproveMissController.php
@@ -96,6 +96,7 @@ class ApproveMissController extends AdminController
             'photo_full_body' => $model->photo_full_body,
             'photo_full_body_2' => $model->photo_full_body_2,
             'video_path' => $this->getOptimizedVideoPath($model->video_path),
+            'video_path_original' => $model->video_path,
             'submitted_at' => $model->submitted_at,
             'created_at' => $model->created_at,
         );
