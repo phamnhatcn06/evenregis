68e2e3fc97a5660af42789cc561db6830ae53459
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-07-14 09:36:21 +0700

    Auto-commit: 2026-07-14_09:36:21

--- protected/modules/admin/views/sportTeams/admin.php
+++ protected/modules/admin/views/sportTeams/admin.php
@@ -184,7 +184,7 @@ Yii::app()->clientScript->registerScript('sport-teams-init', '
     window.BASE_URL = "' . Yii::app()->createUrl('/') . '";
 ', CClientScript::POS_END);
 Yii::app()->clientScript->registerScriptFile(
-    Yii::app()->theme->baseUrl . '/assets/js/pages/sport-teams-overview.js?v=1.0.4',
+    Yii::app()->theme->baseUrl . '/assets/js/pages/sport-teams-overview.js?v=1.0.5',
     CClientScript::POS_END
 );
 ?>
