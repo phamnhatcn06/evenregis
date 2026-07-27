2b744b0dcac046b84811644cf8bb37b25025d133
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-07-27 13:27:56 +0700

    Auto-commit: 2026-07-27_13:27:56

--- protected/views/mail/registration_confirmation_pdf.php
+++ protected/views/mail/registration_confirmation_pdf.php
@@ -87,7 +87,7 @@
 
         .group-title {
             font-weight: bold;
-            font-size: 11px;
+            font-size: 11pt;
             color: #0f172a;
             margin-top: 10px;
             margin-bottom: 5px;
