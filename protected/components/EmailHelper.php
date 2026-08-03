45fda08948ed23cf7633485e48857c72dd423d7c
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-03 15:53:44 +0700

    Auto-commit: 2026-08-03_15:53:44

--- protected/components/EmailHelper.php
+++ protected/components/EmailHelper.php
@@ -527,6 +527,7 @@ class EmailHelper
                     'contest_name' => $c->contest_name,
                     'position_name' => isset($info['position_name']) ? $info['position_name'] : '',
                     'division_name' => isset($info['division_name']) ? $info['division_name'] : '',
+                    'start_working_date' => isset($info['end_starting_date']) ? $info['end_starting_date'] : '',
                     'photo_path' => self::resolveAttendeePhoto($info),
                 );
             }
