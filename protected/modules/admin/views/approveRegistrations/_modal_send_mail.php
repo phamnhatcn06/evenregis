b0da6dbcdf7a15060763e70f6531f650e77fd052
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-05 15:37:47 +0700

    Auto-commit: 2026-08-05_15:37:47

--- protected/modules/admin/views/approveRegistrations/_modal_send_mail.php
+++ protected/modules/admin/views/approveRegistrations/_modal_send_mail.php
@@ -33,7 +33,7 @@
                         <div>
                             Nội dung mail sẽ tự động kèm theo danh sách chi tiết:
                             <ul class="mb-0 ps-3 mt-1">
-                                <li><strong>Đợt 1</strong>: Bảng các đội thi tương ứng các môn thể thao và danh sách VĐV.</li>
+                                <li><strong>Đợt 1</strong>: Bảng các đội thi theo môn thể thao kèm danh sách VĐV, danh sách thí sinh Miss và các tiết mục văn nghệ.</li>
                                 <li><strong>Đợt 2</strong>: Bảng các môn thi nghiệp vụ và danh sách thí sinh dự thi.</li>
                             </ul>
                         </div>
