94d216cd6f5e8a7f9fc9a8e0f24db81b82a29908
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-05 11:54:55 +0700

    Auto-commit: 2026-08-05_11:54:55

--- protected/models/SportTeams.php
+++ protected/models/SportTeams.php
@@ -32,7 +32,7 @@ class SportTeams extends BaseSportTeams
     public function rules()
     {
         $rules = parent::rules();
-        $rules[] = array('event_name, sport_name, property_name, property_code, registration_name, alliance_org_names, alliance_org_ids, member_count, team_name, is_alliance, alliance_property_ids, status, registration_id', 'safe');
+        $rules[] = array('event_name, sport_name, property_name, property_code, registration_name, alliance_org_names, alliance_org_ids, member_count, team_name, is_alliance, is_alliance_team, alliance_property_ids, status, registration_id', 'safe');
         return $rules;
     }
 
