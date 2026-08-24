7de6453ef6b8171045c7751941383104a03cb380
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-24 09:04:38 +0700

    Auto-commit: 2026-08-24_09:04:38

--- protected/modules/admin/controllers/ApproveRegistrationsController.php
+++ protected/modules/admin/controllers/ApproveRegistrationsController.php
@@ -1273,6 +1273,14 @@ class ApproveRegistrationsController extends AdminController
                     'role_name' => $r['role_name'],
                 );
             }
+            $affectedTalents = array();
+            foreach ($summary['talent_entries'] as $te) {
+                $affectedTalents[] = array(
+                    'entry_id' => $te['entry_id'],
+                    'entry_title' => $te['entry_title'],
+                    'category_name' => $te['category_name'],
+                );
+            }
 
             AttendeeReplacements::record(array(
                 'registration_id' => $attendee->registration_id,
@@ -1286,6 +1294,7 @@ class ApproveRegistrationsController extends AdminController
                     'sports' => $affectedSports,
                     'competitions' => $affectedCompetitions,
                     'beauty_contests' => $affectedBeautyContests,
+                    'talents' => $affectedTalents,
                     'roles' => $affectedRoles,
                 ),
                 'cancelled_teams' => $withdrawCancelledTeams,
