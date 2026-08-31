30306cd32e550479aabb5cb9e3f1c61e2ccc29e6
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-31 11:36:12 +0700

    Auto-commit: 2026-08-31_11:36:11

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -132,13 +132,13 @@ $loginUrl = $portalUrl . '/login?redirect=' . urlencode($returnUrl);
             <div class="flash-message flash-info"><?php echo Yii::app()->user->getFlash('info'); ?></div>
         <?php endif; ?>
 
-        <a href="https://portal.muongthanh.vn" class="btn-portal">
+        <a href="<?php echo CHtml::encode($loginUrl); ?>" class="btn-portal">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="20" height="20">
                 <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
                 <polyline points="10,17 15,12 10,7" />
                 <line x1="15" y1="12" x2="3" y2="12" />
             </svg>
-            Đăng nhập với Portal
+            Đăng nhập nội bộ
         </a>
     </div>
 </body>
