77bf0b1940df044c7fa5d84cc1194ecad719261c
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-09-18 14:09:02 +0700

    Auto-commit: 2026-09-18_14:09:02

--- protected/modules/admin/views/competitionRegistrations/overview.php
+++ protected/modules/admin/views/competitionRegistrations/overview.php
@@ -6,6 +6,13 @@ $this->breadcrumbs = array(
 
 $this->menu = array(
     array(
+        'label' => 'Chọn vào chung kết',
+        'url' => $this->createUrl('final'),
+        'color' => 'warning',
+        'icon' => 'fa-trophy',
+        'id' => 'btn_final',
+    ),
+    array(
         'label' => 'Danh sách đăng ký',
         'url' => $this->createUrl('admin'),
         'color' => 'secondary',
