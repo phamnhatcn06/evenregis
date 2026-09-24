6b36491cf7b5568c5a3cb61f0a98036db76386ec
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-09-25 01:04:16 +0700

    Auto-commit: 2026-09-25_01:04:16

--- protected/models/RegistrationPeriods.php
+++ protected/models/RegistrationPeriods.php
@@ -84,8 +84,8 @@ class RegistrationPeriods extends BaseRegistrationPeriods
 		} elseif (isset($data['event_name'])) {
 			$this->event_name = $data['event_name'];
 		}
-		if (isset($data['period_type'])) {
-			$this->period_type = $data['period_type'];
+		if (isset($data['is_final'])) {
+			$this->is_final = (int) $data['is_final'];
 		}
 	}
 
