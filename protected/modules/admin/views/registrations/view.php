d4108889e30c6c5625959e901621df46dff22d5d
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-09-25 10:07:44 +0700

    Auto-commit: 2026-09-25_10:07:43

--- protected/modules/admin/views/registrations/view.php
+++ protected/modules/admin/views/registrations/view.php
@@ -1022,7 +1022,7 @@ $canAddContent = $canEdit && empty($isFinalPeriod);
                             <span class="badge bg-danger rounded-pill ms-2"><?php echo $missPendingCount; ?> yêu cầu</span>
                         <?php endif; ?>
                     </h5>
-                    <?php if ($canEdit): ?>
+                    <?php if ($canAddContent): ?>
                         <button type="button" class="btn btn-sm btn-primary text-white" data-bs-toggle="modal" data-bs-target="#addMissModal">
                             <i class="fa fa-plus me-1"></i>Đăng ký
                         </button>
